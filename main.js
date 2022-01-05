a1e6db1e1bcdd0fcf861934a82fbf1cd3485b13a
coder519 <95204792+Coder519@users.noreply.github.com>
2022-01-05 13:07:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+          document.getElementById("textbox").innerHTML="";
+          recognition.start();
+}
+
+recognition.onresult = function run (event) {
+          console.log(event);
+          var Content = event.results[0] [0].transcript;
+          console.log(Content);
+
+          document.getElementById("textbox").innerHTML=Content;
+}
